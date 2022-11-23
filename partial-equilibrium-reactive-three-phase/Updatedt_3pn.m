c6a43ea16b15d9ce5ca258329f301ff2775a90b8
wangyufei1989 <97456379+wangyufei1989@users.noreply.github.com>
2022-11-23 16:05:43 +0100

    Add files via upload

---
+++ partial-equilibrium-reactive-three-phase/Updatedt_3pn.m
@@ -0,0 +1,33 @@
+function dt=Updatedt_3pn(state,dt,DT)
+
+
+
+
+
+
+    dpm=100000*50;
+dsm=1e-2;
+
+state.ds=abs(state.ds);
+state.dp=abs(state.dp);
+
+a=1;
+
+dt=dt*min([1.5,0.5*abs(dpm/max(max(state.dp(:,1)),max(state.dp(:,1))))*a]);
+%dt=dt*min([1.5,0.5*abs(dpm/max(max(state.dp(:,1),state.dp(:,2))))*a,0.5*abs(dsm/max(state.ds(:,1)))]);
+
+
+dt=min([dt, state.CFL*4e-1,2e5]);
+
+ if size(DT,1)>100 &&all(DT(end-20:end)<5) 
+     dt=rand(1,1)*state.meandt*2;
+ end
+%for compressible insoluble 0.00001;
+%for incompressible 0.001
+%for compressible soluble0.01
+
+%dt=dt*min([1,dpm/max(state.dp)]);
+
+
+end
+
